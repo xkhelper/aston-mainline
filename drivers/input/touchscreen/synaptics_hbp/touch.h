510b390e5b9b6fd4b4de05a95953030ad24dc1fb
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2024-11-06 07:50:28 +0300

    aston: Touchscreen: Fix build N.6

--- drivers/input/touchscreen/synaptics_hbp/touch.h
+++ drivers/input/touchscreen/synaptics_hbp/touch.h
@@ -14,14 +14,11 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include "../tp_devices.h"
-#include "../touchpanel_common.h"
+#include "touchpanel_common.h"
 
 
 extern bool tp_judge_ic_match(char *tp_ic_name);
-#ifndef CONFIG_REMOVE_OPLUS_FUNCTION
-extern int tp_util_get_vendor(struct hw_resource *hw_res, struct panel_info *panel_data);
-#endif
+
 extern int preconfig_power_control(struct touchpanel_data *ts);
 
 extern int reconfig_power_control(struct touchpanel_data *ts);
