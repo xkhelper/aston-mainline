fa638b3d485b1eb52327c1092859834e9cc83ea9
Your Name <you@example.com>
2025-04-11 15:03:27 +0300

    ath12k: Force Single Chip MLO support to fix firmware boot

--- drivers/net/wireless/ath/ath12k/qmi.c
+++ drivers/net/wireless/ath/ath12k/qmi.c
@@ -2265,9 +2265,7 @@ static void ath12k_qmi_phy_cap_send(struct ath12k_base *ab)
 		goto out;
 	}
 
-	if (resp.single_chip_mlo_support_valid &&
-	    resp.single_chip_mlo_support)
-		ab->single_chip_mlo_supp = true;
+	ab->single_chip_mlo_supp = true;
 
 	if (!resp.num_phy_valid) {
 		ret = -ENODATA;
